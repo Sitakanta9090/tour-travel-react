d5edb5aca8aa882eaae7866dd924ca5aefd70cce
Sitakanta Malik <sitakantamalik2346@gmail.com>
2024-01-25 18:22:46 +0530

    Tour & travels first commit

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./main.css"
+
+function Main() {
+  return (
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 className='title'>
+          Most Visited Destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+
+      </div>
+    </section>
+  )
+}
+
+export default Main
